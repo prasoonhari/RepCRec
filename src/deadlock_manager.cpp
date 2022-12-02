81f4e14ee992ef69f6d567ab5f5e0a4b11832ee3
Aditya Upadhyaya <aditupadhyaya@gmail.com>
2022-12-02 11:36:14 +0530

    cycle

--- src/deadlock_manager.cpp
+++ src/deadlock_manager.cpp
@@ -1,9 +1,54 @@
 
 #include "deadlock_manager.hpp"
+#include <stack>
 
 using namespace std;
+
 
 DeadLockManager::DeadLockManager() {
 
 
+}
+
+
+vector<int> DeadLockManager::deadlockDetector(map<int, vector<int> > adjList) {
+    map<int, int> visited;
+    for (auto item : adjList) {
+        visited[item.first] = 0;
+    }
+    
+    for (auto item : visited) {
+        if (item.second == 0) {
+            stack<int> tracker;
+            vector<int> res = dfs(item.first, adjList, visited, tracker);
+            if (res.size() > 0) return res;
+        }
+    }
+
+    return vector<int>();
+}
+
+vector<int> dfs(int vertex, map<int, vector<int> > adjList, map<int, int > visited, stack<int> tracker) {
+    visited[vertex] = -1;
+
+    for (auto next : adjList[vertex]) {
+        if (visited[next] == -1) {
+             // cycle
+             vector<int> buffer;
+             while(!tracker.empty()) {
+                buffer.push_back(tracker.top());
+                tracker.pop();
+             }
+             return buffer;
+        } else if (visited[next] == 0) {
+            tracker.push(next);
+            vector<int> res = dfs(next, adjList, visited, tracker);
+            if (res.size() > 0) return res;
+
+        }
+    }
+
+    tracker.pop();
+    visited[vertex] = 1;
+    return vector<int>();
 }
