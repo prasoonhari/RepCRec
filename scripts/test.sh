bcb37879a46f24c9dbc8f0c6e1e86047a4beea04
Aditya Upadhyaya <aditupadhyaya@gmail.com>
2022-12-03 07:14:04 +0530

    testing script

---
+++ scripts/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+INDIR=$1
+OUTDIR=$2
+PROG=$3
+
+INPUTS=`seq 1 21`
+
+
+for I in ${INPUTS}; do
+	echo ${I}
+	OUTF="${OUTDIR}/out${I}"
+	echo "${PROG} ${INDIR}/in${I} ${OUTF}"
+	${PROG} ${INDIR}/in${I} ${OUTF}
+done
